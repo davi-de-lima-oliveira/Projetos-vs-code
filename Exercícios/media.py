9486f4010b75665807a7fa0da3e5530978086161
davi-de-lima-oliveira <davimegaultra@gmail.com>
2025-12-16 16:32:16 -0300

    cálculo de média

---
+++ Exercícios/media.py
@@ -0,0 +1,6 @@
+números = [10,20,30,40]
+soma = sum(números)
+média = soma / len (números)
+print (" A média será ;" , média)
+# Exercício : Calcular a média de um conjunto de números armazenados em uma lista.
+# Resposta após teste : 25.0 
