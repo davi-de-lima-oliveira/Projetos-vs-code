4b14efd7ad86ec5c15979a36c5e5306659e19067
davi-de-lima-oliveira <davimegaultra@gmail.com>
2025-12-16 16:25:32 -0300

    nova calculadora

---
+++ Exercícios/somadevalores.py
@@ -0,0 +1,12 @@
+soma = 24 + 8
+divisao = 24/ 8
+subtraçao = 24 - 8 
+multiplicaçao = 24 * 8 
+multiplicaçao_exponencial = 24 ** 8
+
+print (soma)
+print (divisao)
+print (subtraçao)
+print (multiplicaçao)
+print (multiplicaçao_exponencial)
+
